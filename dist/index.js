4da7d67fa72cf829eaecb2c3a90e9ef1a6b3177c
Liam Nichols <liam.nichols.ln@gmail.com>
2022-04-23 15:58:56 +0200

    Fix some logging issues

--- dist/index.js
+++ dist/index.js
@@ -17009,20 +17009,21 @@ const main = async () => {
         const privateKey = core.getInput('private_key', { required: true })
 
         // Create the App
-        core.info(`Creating app $(appId)`)
+        core.info(`Creating app ${appId}`)
         const app = new App({
             appId: appId,
             privateKey: privateKey
         })
 
         // Mint a token for the given installationId
-        core.info(`Minting access token for installation $(installationId)`)
+        core.info(`Minting access token for installation ${installationId}`)
         const octokit = await app.getInstallationOctokit(installationId)
         const { token } = await octokit.auth({ type: 'installation' })
 
         // Mask the token and set the output
         core.setSecret(token)
         core.setOutput('token', token)
+        core.info('Token generated successfully')
 
     } catch (error) {
       core.setFailed(error.message)
